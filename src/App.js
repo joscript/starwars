4bc6d2f98d5c2fa3bd63957365d958420e91bee6
josuaLagat <josua.lagat@volenday.com>
2021-02-15 10:07:58 +0800

    fetching planets and people using useQuery. Create components for planets and people

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import Navbar from "../src/components/Navbar";
 import Planets from "../src/components/Planets";
 import People from "../src/components/People";
 
+const queryClient = new QueryClient();
+
 function App() {
   const [page, setPage] = useState("planets");
   return (
-    <div className='App'>
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className='content'>
-        {page === "Planets" ? <Planets /> : <People />}
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className='content'>
+          {page === "Planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
